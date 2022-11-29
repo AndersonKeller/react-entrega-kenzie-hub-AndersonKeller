b3c1aaf4a81f048c3695baa7d6970f1d47798448
Anderson Keller <andersonkellervendas@hotmail.com>
2022-11-29 14:17:19 -0300

    configure axios, and routes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Dashboard } from "./pages/Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://kenziehub.herokuapp.com",
+  timeout: 3000,
+});
