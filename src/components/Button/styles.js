9e93ace438fd8cf8a66528673f4d849c36ee4313
Anderson Keller <andersonkellervendas@hotmail.com>
2022-11-30 11:00:40 -0300

    create button goRegiser

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -8,6 +8,11 @@ export const StyledButton = styled.button`
           background-color: var(--color-primary);
           color: var(--color-gray0);
         `
+      : "goRegister"
+      ? css`
+          background-color: var(--color-gray1);
+          color: var(--color-gray0);
+        `
       : css`
           background-color: var(--color-gray2);
           color: var(--color-gray0);
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,8 @@
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { StyledForm } from "../../styles/StyledForm";
+import { StyledButton } from "../../components/Button/styles";
+import { Navigate } from "react-router-dom";
 
 export function Login() {
   return (
@@ -13,6 +15,10 @@ export function Login() {
         <label htmlFor="password">Senha</label>
         <input id="password" type="password" placeholder="Digite sua senha" />
         <Button type={"submit"} color={"default"} text={"Entrar"}></Button>
+        <p>Ainda não possui uma conta?</p>
+        <StyledButton type={"button"} color={"goRegister"} text={"Cadastre-se"}>
+          Cadastre-se
+        </StyledButton>
       </StyledForm>
     </>
   );
